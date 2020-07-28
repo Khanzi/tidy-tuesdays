c902bd4c4ec305a14554566269e2167bbb2445d6
Kahlil Wehmeyer <kahlilwehmeyer@gmai.com>
2020-07-28 14:27:34 -0400

    completed visualiztion

--- 2020-07-28/penguins.r
+++ 2020-07-28/penguins.r
@@ -1,6 +1,6 @@
 
 # Libraries ---------------------------------------------------------------
-
+setwd("~/Documents/tidy-tuesdays/2020-07-28")
 library(tidyverse); theme_set(theme_minimal())
 library(ggbeeswarm)
 library(janitor)
@@ -8,18 +8,14 @@ library(caret)
 library(cowplot)
 library(ggpubr)
 library(png)
-library(tidytuesdayR)
 
 
 # Getting the data --------------------------------------------------------
 
-tuesdata <- tt_load('2020-07-28')
 
-penguins <- tuesdata$penguins %>% clean_names()
+penguins <- read_csv('penguins.csv') 
 
-penguins_raw <- tuesdata$penguins_raw %>% clean_names()
 
-penguins_raw$island <- penguins_raw$island %>% factor()
 
 
 
@@ -35,30 +31,30 @@ penguins %>%
   geom_point(aes(bill_depth_mm, bill_length_mm, color = sex, alpha = 0.1)) +
   
   # Labels
-  xlab("Bill Depth (mm)") + ylab("Bill Length (mm)") +
-  labs(title = "Bill Sizes vs Gender") +
-  labs(subtitle = "Adelie (Pygoscelis adeliae)") + 
+  theme(legend.position = "none",
+        axis.title.x = element_blank()) +
+  ylab("Bill Length (mm)") +
   
   
   # Annotation
   
   ## Drawing a line through gender separation
   annotate("segment",
-           x = 16, xend = 21, y = 44, yend = 34,
-           color = "gray",
+           x = 16.4, xend = 21, y = 43.4, yend = 34,
+           color = "black",
            linetype = 2)  +
   
   ## Labeling Line
   annotate("text",
            x = 16.5, y = 44.5,
            label = "gender separation",
-           color = "darkgray") +
+           color = "black") +
   
   ## Labeling male portion
   annotate("text",
            x = 20 , y = 44,
            label = "males",
-           color = "darkgray",
+           color = "black",
            size = 5) +
   
 
@@ -66,7 +62,7 @@ penguins %>%
   annotate("text",
            x = 16 , y = 34,
            label = "females",
-           color = "darkgray",
+           color = "black",
            size = 5) -> p1
 
 ## Gentoo
@@ -76,30 +72,28 @@ penguins %>%
   geom_point(aes(bill_depth_mm, bill_length_mm, color = sex, alpha = 0.1)) +
   
   # Labels
-  xlab("Bill Depth (mm)") + ylab("Bill Length (mm)") +
-  labs(title = "Bill Sizes vs Gender") +
-  labs(subtitle = "Gentoo (Pygoscelis papua)") + 
-  
+  theme(legend.position = "none") +
+  theme(axis.title = element_blank()) + 
   
   # Annotation
   
   ## Drawing a line through gender separation
   annotate("segment",
            x = 14, xend = 15.4, y = 53.4, yend = 43,
-           color = "gray",
+           color = "black",
            linetype = 2)  +
   
   ## Labeling Line
   annotate("text",
            x = 14, y = 55,
            label = "gender separation",
-           color = "darkgray") +
+           color = "black") +
   
   ## Labeling male portion
   annotate("text",
            x = 16.5 , y = 55,
            label = "males",
-           color = "darkgray",
+           color = "black",
            size = 5) +
   
 
@@ -107,7 +101,7 @@ penguins %>%
   annotate("text",
            x = 13.8 , y = 43,
            label = "females",
-           color = "darkgray",
+           color = "black",
            size = 5) -> p2
 
 
@@ -118,30 +112,28 @@ penguins %>%
   geom_point(aes(bill_depth_mm, bill_length_mm, color = sex, alpha = 0.1)) +
   
   # Labels
-  xlab("Bill Depth (mm)") + ylab("Bill Length (mm)") +
-  labs(title = "Bill Sizes vs Gender") +
-  labs(subtitle = "Chinstrap (Pygoscelis antarctica)") + 
-  
+  theme(legend.position = "none") +
+  theme(axis.title = element_blank()) +
   
   # Annotation
   
   ## Drawing a line through gender separation
   annotate("segment",
            x = 17, xend = 19, y = 53.4, yend = 43,
-           color = "gray",
+           color = "black",
            linetype = 2) +
   
   ## Labeling Line
   annotate("text",
            x = 17, y = 55,
            label = "gender separation",
-           color = "darkgray") +
+           color = "black") +
   
   ## Labeling male portion
   annotate("text",
            x = 20, y = 55,
            label = "males",
-           color = "darkgray",
+           color = "black",
            size = 5) +
   
 
@@ -149,7 +141,7 @@ penguins %>%
   annotate("text",
            x = 17, y = 43,
            label = "females",
-           color = "darkgray",
+           color = "black",
            size = 5) -> p3
 ###
 
@@ -186,90 +178,118 @@ clean
 
 
 # Plotting inferred vars --------------------------------------------------
+clean$inferred <- as.numeric(clean$inferred)
+clean %>% mutate(sex = paste0(sex, '_', inferred)) ->  clean
 
 clean %>% filter(species == "Adelie") %>% 
   ggplot() +
-  geom_point(aes(bill_length_mm, bill_depth_mm , size = inferred, color=sex, alpha = 0.1, shape = inferred)) +
+  geom_point(aes(y = bill_length_mm,  x = bill_depth_mm , size = inferred, color=sex, alpha = 0.1)) +
   
   # Labels and Titles
-  xlab("Bill Length (mm)") + ylab("Bill Depth (mm)") +
-  labs(title = "Prediction for Sex", subtitle = "Adelie Penguins") +
-  
+  theme(legend.position = "none") +
+  theme(axis.title.x = element_blank()) + 
+  ylab("Bill Length (mm)") +
   
   # Annotations
   
   ## Arrow to inferred female
   annotate("curve",
-           x = 32.3, xend = 33.5,
-           y = 17, yend = 18,
+           y = 36, yend = 37.6,
+           x = 16.2, xend = 16.9,
            curvature = -0.3,
            arrow = arrow(
               length = unit(2, 'mm'),
               type = 'closed'),
-           color = "gray"
+           color = "black"
            ) +
   
   ## Inferred female label
   annotate("text",
-           x = 33.3, y=16.5,
+           y = 34.3, x=16.5,
            label = "reasonable prediction\nfor female",
-           color = "gray") +
+           color = "black") +
   
   ## Inferred male arrow
   annotate("curve",
-           xend = 42.4, x = 44,
-           yend = 20.1, y = 20.5,
+           yend = 42, y = 42.2,
+           xend = 20.4, x = 21,
            curvature = -0.3,
            arrow = arrow(length = unit(2,'mm'), type = 'closed'),
-           color = "gray"
+           color = "black"
            ) +
   
   ## Inferred male label
   annotate("text",
-           x = 44, y = 21,
+           y = 44, x = 21,
            label = "reasonable prediction\nfor male",
-           color = "gray") -> p4
+           color = "black") -> p4
 
 clean %>% filter(species == "Gentoo") %>% 
   ggplot() +
-  geom_point(aes(bill_length_mm, bill_depth_mm , size = inferred, color=sex, alpha = 0.1, shape = inferred)) +
+ 
+  geom_point(aes(y = bill_length_mm,x = bill_depth_mm , size = inferred, color=sex, alpha = 0.1)) +
+  
+  theme(legend.position = "none") +
+  theme(axis.title.y = element_blank()) +
+  theme(axis.title.x = element_text(size = 20)) + 
+  scale_color_manual(values = c("#F8766D", "#7CAE00","#00BFC4" ,"#F8766D")) +
+  xlab("Bill Depth (mm)") +
+  
+  annotate("text",
+           label = "incorrect female prediction",
+           x = 16.5, y = 40.5) +
+  
+  annotate("curve",
+           x = 16.4, xend = 15.8,
+           y = 41, yend = 44.4,
+           arrow = arrow(length = unit(2, 'mm'), type = 'closed')) ->p5
   
   # Labels and Titles
-  xlab("Bill Length (mm)") + ylab("Bill Depth (mm)") +
-  labs(title = "Prediction for Sex", subtitle = "Gentoo Penguins") -> p5
-
-ggdraw() + draw_text("No missing sex data for Chinstraps", size = 24) -> p6
+ggdraw() + 
+  draw_text("No missing data for Chinstraps", size = 30, x = 0.5, y = 0.5) +
+  draw_text("@Khanzi_w on Twitter for #TidyTuesday | Data: Dr. Kristen Gorman | Original Art: @alaphair on Instagram",
+            size = 10,
+            x = 0.5, y = 0.05) -> p6
 
 # -------------------------------------------------------------------------
 
 ggdraw() +
   draw_image("adelie.png") +
-  draw_text("Adelie", y = 0.93, size = 40, color = "white") -> adelie_img
+  draw_text("Adelie", y = 0.92, size = 34, color = "white") -> adelie_img
 
 ggdraw() +
   draw_image("gentoo.png") +
-  draw_text("Gentoo", y = 0.93, size = 40, color = "white") -> gentoo_img
+  draw_text("Gentoo", y = 0.92, size = 34, color = "white") -> gentoo_img
 
 ggdraw() +
   draw_image("chinstrap.png") +
-  draw_text("Chinstrap", y = 0.93, size = 40, color = "white") -> chinstrap_img
+  draw_text("Chinstrap", y = 0.92, size = 34, color = "white") -> chinstrap_img
 # Cowplot -----------------------------------------------------------------
 
 ggdraw() +
-  draw_text("Title", y =0.9, x = 0.1, size = 40) +
-  draw_text("subtitle", y = 0.8, x = 0.11, size = 30)
+  draw_text("Predicting Sex | Palmer Penguins", x= 0.5, y = 0.85, size = 40) +
+  draw_text("LDA Model (Accuracy = 0.91, 10-fold cross validated)", x = 0.5, y = 0.60, size = 20) -> header_plot
 
+header <- plot_grid(header_plot, ncol=1)
+image_row <- plot_grid(adelie_img, gentoo_img, chinstrap_img, ncol=3)
+row1 <- plot_grid(p1,p2,p3, ncol=3)
+row2 <- plot_grid(p4,p5,p6, ncol=3)
 
 plot_grid(
-          adelie_img, gentoo_img, chinstrap_img,
-  
-          p1,p2,p3,
-          p4, p5, p6,
+          header, 
+          image_row,
+          
+          row1,
+          
+          row2,
+          
+          rel_heights = c(1.5,2,2.5,2.5),
+          nrow = 4
+)
           
           
-          ncol = 3) 
 
-# TODO: Fix annotation; make them darker
+# TODO: Fix annotation; make them darker./ DONE :)
 # TODO: Add title and description
 # TODO: Clean up legends 
 # TODO: Tidy up theme
